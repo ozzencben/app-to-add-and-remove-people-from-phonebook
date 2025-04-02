aa815bf43c3f4b6dc6cf1d6996bcb98a2fead88d
özenç <ozzencben@gmail.com>
2025-04-02 16:59:20 +0300

    Add files via upload

---
+++ Phone Directory/Directory/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Directory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.clearbtn = new System.Windows.Forms.Button();
+            this.uptadebtn = new System.Windows.Forms.Button();
+            this.deletebtn = new System.Windows.Forms.Button();
+            this.addbtn = new System.Windows.Forms.Button();
+            this.phonemasktxt = new System.Windows.Forms.MaskedTextBox();
+            this.mailtxt = new System.Windows.Forms.TextBox();
+            this.lasttxt = new System.Windows.Forms.TextBox();
+            this.nametxt = new System.Windows.Forms.TextBox();
+            this.ıdtxt = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(439, 390);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Directory";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(433, 365);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.clearbtn);
+            this.groupBox2.Controls.Add(this.uptadebtn);
+            this.groupBox2.Controls.Add(this.deletebtn);
+            this.groupBox2.Controls.Add(this.addbtn);
+            this.groupBox2.Controls.Add(this.phonemasktxt);
+            this.groupBox2.Controls.Add(this.mailtxt);
+            this.groupBox2.Controls.Add(this.lasttxt);
+            this.groupBox2.Controls.Add(this.nametxt);
+            this.groupBox2.Controls.Add(this.ıdtxt);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(457, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(285, 390);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Yeni Kişi";
+            // 
+            // clearbtn
+            // 
+            this.clearbtn.Location = new System.Drawing.Point(102, 327);
+            this.clearbtn.Name = "clearbtn";
+            this.clearbtn.Size = new System.Drawing.Size(161, 30);
+            this.clearbtn.TabIndex = 8;
+            this.clearbtn.Text = "CLEAR";
+            this.clearbtn.UseVisualStyleBackColor = true;
+            this.clearbtn.Click += new System.EventHandler(this.clearbtn_Click);
+            // 
+            // uptadebtn
+            // 
+            this.uptadebtn.Location = new System.Drawing.Point(102, 291);
+            this.uptadebtn.Name = "uptadebtn";
+            this.uptadebtn.Size = new System.Drawing.Size(161, 30);
+            this.uptadebtn.TabIndex = 7;
+            this.uptadebtn.Text = "UPDATE";
+            this.uptadebtn.UseVisualStyleBackColor = true;
+            this.uptadebtn.Click += new System.EventHandler(this.uptadebtn_Click);
+            // 
+            // deletebtn
+            // 
+            this.deletebtn.Location = new System.Drawing.Point(102, 255);
+            this.deletebtn.Name = "deletebtn";
+            this.deletebtn.Size = new System.Drawing.Size(161, 30);
+            this.deletebtn.TabIndex = 6;
+            this.deletebtn.Text = "DELETE";
+            this.deletebtn.UseVisualStyleBackColor = true;
+            this.deletebtn.Click += new System.EventHandler(this.deletebtn_Click);
+            // 
+            // addbtn
+            // 
+            this.addbtn.Location = new System.Drawing.Point(102, 219);
+            this.addbtn.Name = "addbtn";
+            this.addbtn.Size = new System.Drawing.Size(161, 30);
+            this.addbtn.TabIndex = 5;
+            this.addbtn.Text = "ADD";
+            this.addbtn.UseVisualStyleBackColor = true;
+            this.addbtn.Click += new System.EventHandler(this.addbtn_Click);
+            // 
+            // phonemasktxt
+            // 
+            this.phonemasktxt.Location = new System.Drawing.Point(102, 140);
+            this.phonemasktxt.Name = "phonemasktxt";
+            this.phonemasktxt.Size = new System.Drawing.Size(161, 26);
+            this.phonemasktxt.TabIndex = 3;
+            // 
+            // mailtxt
+            // 
+            this.mailtxt.Location = new System.Drawing.Point(102, 172);
+            this.mailtxt.Name = "mailtxt";
+            this.mailtxt.Size = new System.Drawing.Size(161, 26);
+            this.mailtxt.TabIndex = 4;
+            // 
+            // lasttxt
+            // 
+            this.lasttxt.Location = new System.Drawing.Point(102, 108);
+            this.lasttxt.Name = "lasttxt";
+            this.lasttxt.Size = new System.Drawing.Size(161, 26);
+            this.lasttxt.TabIndex = 2;
+            // 
+            // nametxt
+            // 
+            this.nametxt.Location = new System.Drawing.Point(102, 79);
+            this.nametxt.Name = "nametxt";
+            this.nametxt.Size = new System.Drawing.Size(161, 26);
+            this.nametxt.TabIndex = 1;
+            // 
+            // ıdtxt
+            // 
+            this.ıdtxt.Enabled = false;
+            this.ıdtxt.Location = new System.Drawing.Point(102, 45);
+            this.ıdtxt.Name = "ıdtxt";
+            this.ıdtxt.Size = new System.Drawing.Size(161, 26);
+            this.ıdtxt.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(48, 178);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 18);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Mail :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 18);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Phone :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 18);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Lastname :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Name :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(754, 414);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Directory";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.MaskedTextBox phonemasktxt;
+        private System.Windows.Forms.TextBox ıdtxt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button clearbtn;
+        private System.Windows.Forms.Button uptadebtn;
+        private System.Windows.Forms.Button deletebtn;
+        private System.Windows.Forms.Button addbtn;
+        private System.Windows.Forms.TextBox mailtxt;
+        private System.Windows.Forms.TextBox lasttxt;
+        private System.Windows.Forms.TextBox nametxt;
+    }
+}
+
---
+++ Phone Directory/Directory/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Directory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection connection = new SqlConnection(@"Data Source=SOL-9\SQLEXPRESS;Initial Catalog=Directory;Integrated Security=True;TrustServerCertificate=True");
+
+        void getList()
+        {
+            DataTable table = new DataTable();
+            SqlDataAdapter dataAdapter = new SqlDataAdapter("Select * from Persons", connection);
+            dataAdapter.Fill(table);
+            dataGridView1.DataSource = table;
+        }
+
+        void clear()
+        {
+            nametxt.Text = "";
+            lasttxt.Text = "";
+            phonemasktxt.Text = "";
+            mailtxt.Text = "";
+            nametxt.Focus();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getList();
+        }
+
+        private void addbtn_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand cmd = new SqlCommand("insert into Persons (Name,Lastname,Phone,Mail) values (@p1,@p2,@p3,@p4)", connection);
+            cmd.Parameters.AddWithValue("@p1", nametxt.Text);
+            cmd.Parameters.AddWithValue("@p2", lasttxt.Text);
+            cmd.Parameters.AddWithValue("@p3", phonemasktxt.Text);
+            cmd.Parameters.AddWithValue("@p4", mailtxt.Text);
+            cmd.ExecuteNonQuery();
+            connection.Close();
+            getList();
+            MessageBox.Show("Person added to contacts","Add Person",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            clear();
+        }
+
+        private void clearbtn_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int chosen = dataGridView1.SelectedCells[0].RowIndex;
+            ıdtxt.Text = dataGridView1.Rows[chosen].Cells[0].Value.ToString();
+            nametxt.Text = dataGridView1.Rows[chosen].Cells[1].Value.ToString();
+            lasttxt.Text = dataGridView1.Rows[chosen].Cells[2].Value.ToString();
+            phonemasktxt.Text = dataGridView1.Rows[chosen].Cells[3].Value.ToString();
+            mailtxt.Text = dataGridView1.Rows[chosen].Cells[4].Value.ToString();
+        }
+
+        private void deletebtn_Click(object sender, EventArgs e)
+        {
+            DialogResult desicion = MessageBox.Show("Are you sure you want to delete the contact?", "Delete Person", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (desicion == DialogResult.Yes)
+            {
+                connection.Open();
+                SqlCommand cmdI = new SqlCommand("Delete from Persons where ID =" + ıdtxt.Text, connection);
+                cmdI.ExecuteNonQuery();
+                connection.Close();
+                getList();
+                MessageBox.Show("The person was deleted from the contact list.", "Delete Person", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Deletion canceled", "Delete Person", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+        private void uptadebtn_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand cmdII = new SqlCommand("update Persons set Name = @p1, Lastname = @p2, Mail = @p3, Phone = @p4 where ID = @p5", connection);
+            cmdII.Parameters.AddWithValue("@p1", nametxt.Text);
+            cmdII.Parameters.AddWithValue("@p2", lasttxt.Text);
+            cmdII.Parameters.AddWithValue("@p3", mailtxt.Text);
+            cmdII.Parameters.AddWithValue("@p4", phonemasktxt.Text);
+            cmdII.Parameters.AddWithValue("@p5", ıdtxt.Text);
+            cmdII.ExecuteNonQuery();
+            connection.Close();
+            getList();
+            MessageBox.Show("Person updated successfully", "Update Person", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            clear();
+        }
+    }
+}
